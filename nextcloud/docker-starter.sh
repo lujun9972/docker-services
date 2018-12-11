cc066873bd5bbc6bd91cfda89029e16a4b6ae955
DarkSun <lujun9972@gmail.com>
2018-12-11 21:38:44 +0800

    update

--- nextcloud/docker-starter.sh
+++ nextcloud/docker-starter.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-IFACE=$( ip r | grep "default via" | awk '{ print $5 }' )
-export IP=$( ip a | grep "$IFACE" | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )
-mkdir -p data
-#docker run -d -p 4443:4443 -p 1443:443 -p 1080:80 -v ./data:/data --name nextcloudpi ownyourbits/nextcloudpi-armhf $IP
-IP=${IP} docker-compose up -d
+docker run -d \
+	--name nextcloud \
+	-p 443:443 \
+	-e PUID=${UID} -e PGID=$(id -g) \
+	-v ${PWD}/appdata:/config \
+	-v ${PWD}/data:/data \
+	lsioarmhf/nextcloud-aarch64
