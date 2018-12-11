5ca98c56ec00a08e75f11b0fb5a1fc9978e91ff6
DarkSun <lujun9972@gmail.com>
2018-12-11 22:00:55 +0800

    update

--- nextcloud/docker-starter.sh
+++ nextcloud/docker-starter.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 docker run -d \
 	--name nextcloud \
-	-p 443:443 \
+	-p 1443:443 \
+	-p 1080:80 \
 	-e PUID=${UID} -e PGID=$(id -g) \
 	-v ${PWD}/appdata:/config \
 	-v ${PWD}/data:/data \
