c25a55150ac166a03eb56a70a087448df133204c
darksun <lujun9972@gmail.com>
2018-12-21 07:29:16 +0800

    add leanote

---
+++ 5.leanote.arm64/leanote/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 倒入初始数据
+if ! echo "show dbs" |mongo --host mongo|grep leanote
+then
+	mongorestore -h mongo -d leanote --dir /root/leanote/mongodb_backup/leanote_install_data/
+fi
+bash /root/leanote/bin/run.sh
+# # Copy tar data to `/data/` path
+# cp -n -r /root/leanote /data/
+# rm -rf /root/leanote/*
+
+# echo `date "+%Y-%m-%d %H:%M:%S"`' >>>>>> start leanote service'
+# /data/leanote/bin/run.sh
